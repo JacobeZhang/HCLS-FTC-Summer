0a138493b73793e696b6ce0a861332209fec3ca2
Harry <harrypeng48@gmail.com>
2023-07-26 11:39:49 -0400

    Updated TeleOp!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Nazar/NazarTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Nazar/NazarTeleOp.java
@@ -1,4 +1,44 @@
 package org.firstinspires.ftc.teamcode.Nazar;
 
-public class NazarTeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class NazarTeleOp extends LinearOpMode {
+
+    public DcMotor frontLeft;
+    public DcMotor backLeft;
+    public DcMotor backRight;
+    public DcMotor frontRight;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            double y = gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+
+            frontLeft.setPower(y+x);
+            frontRight.setPower(y-x);
+            backLeft.setPower(y-x);
+            backRight.setPower(y+x);
+
+
+
+        }
+
+    }
+
 }
