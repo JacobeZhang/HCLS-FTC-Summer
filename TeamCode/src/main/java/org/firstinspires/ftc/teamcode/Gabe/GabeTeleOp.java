88b9c254d29e4f9ebdaec6e3606c4a0b65c38a68
Sahasra <Sahasra2008@gmail.com>
2023-07-27 11:04:36 -0400

    MY FIRST COMMIT TELEOP 1/2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gabe/GabeTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gabe/GabeTeleOp.java
@@ -1,4 +1,41 @@
 package org.firstinspires.ftc.teamcode.Gabe;
 
-public class GabeTeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class GabeTeleOp extends LinearOpMode {
+
+    public DcMotor frontLeft;
+    public DcMotor backLeft;
+    public DcMotor frontright;
+    public DcMotor backright;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        backLeft = hardwareMap.get(DcMotor.class, "backleft");
+        frontright = hardwareMap.get(DcMotor.class, "frontright");
+        backright = hardwareMap.get(DcMotor.class, "backright");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            double y = gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+
+            frontLeft.setPower(y+x);
+            frontright.setPower(y-x);
+            backLeft.setPower(y-x);
+            backright.setPower(y+x);
+
+        }
+
+    }
+
 }
