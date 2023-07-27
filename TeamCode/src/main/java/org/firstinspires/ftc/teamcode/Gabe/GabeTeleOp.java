78a867c0db755f8e7ec14a7dde2ab51778314112
Sahasra <Sahasra2008@gmail.com>
2023-07-27 11:43:22 -0400

    updated gabe TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gabe/GabeTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gabe/GabeTeleOp.java
@@ -10,6 +10,7 @@ public class GabeTeleOp extends LinearOpMode {
     public DcMotor backLeft;
     public DcMotor frontright;
     public DcMotor backright;
+    public DcMotor slidesmotor;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -18,21 +19,32 @@ public class GabeTeleOp extends LinearOpMode {
         backLeft = hardwareMap.get(DcMotor.class, "backleft");
         frontright = hardwareMap.get(DcMotor.class, "frontright");
         backright = hardwareMap.get(DcMotor.class, "backright");
+        slidesmotor = hardwareMap.get(DcMotor.class,"slidesmotor");
 
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        slidesmotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         waitForStart();
 
         while(opModeIsActive()) {
 
             double y = gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
+            double r = gamepad1.right_stick_x;
 
-            frontLeft.setPower(y+x);
-            frontright.setPower(y-x);
-            backLeft.setPower(y-x);
-            backright.setPower(y+x);
+            frontLeft.setPower(y+x+r);
+            frontright.setPower(y-x-r);
+            backLeft.setPower(y-x+r);
+            backright.setPower(y+x-r);
+
+            if(gamepad2.y){
+                slidesmotor.setPower(0.75);
+            }else if(slidesmotor.getCurrentPosition()>100){
+
+            }
+
+
+
 
         }
 
