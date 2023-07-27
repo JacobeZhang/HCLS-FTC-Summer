b908f915d90ec544b5643170e2f8fb32fc19d6b6
rafia <lubnikhanam05@gmail.com>
2023-07-27 10:54:17 -0400

    extended LinearOpMode class intialized motors added movement and strafing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ty/TyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ty/TyTeleOp.java
@@ -1,5 +1,37 @@
 package org.firstinspires.ftc.teamcode.ty;
 
-public class TyTeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class TyTeleOp extends LinearOpMode {
+
+
+    public DcMotor frontLeft, backLeft, frontRight, backRight;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            double y = gamepad1.left_stick_y, x = gamepad1.left_stick_x;
+            frontLeft.setPower(y+x);
+            backLeft.setPower(y-x);
+            frontRight.setPower(y-x);
+            backRight.setPower(y+1);
+        }
+
+    }
 
 }
